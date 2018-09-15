1f9c2f5a5581b2f467cea3b138efeb5aff4d0382
Shadowinnothing <shadowinnothing@gmail.com>
2018-09-15 15:10:00 -0600

    Added CSS for chat page

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ io.on('connection', (socket) => {
     // Socket.emit emits to one socket
     // io.emit emits to every socket
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('This is from the server');
+    callback();
   });
 
   socket.on('createLocationMessage', (coords) => {
